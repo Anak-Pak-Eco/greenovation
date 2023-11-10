1b161bbe8ec729c20959d4cb0bff84fb9d03d86f
Leonardo472 <leonardojose040702@gmail.com>
2023-11-10 12:15:19 +0700

    feat: CU-865d83dh2_Implement-Edit-View_Leo-Push-Dulu

--- Greenovation/Presenter/Edit Device V2/EditDeviceV2ViewController.swift
+++ Greenovation/Presenter/Edit Device V2/EditDeviceV2ViewController.swift
@@ -12,6 +12,23 @@ class EditDeviceV2ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupToolbar()
+    }
+    
+    @objc func backButtonTapped() {
+        
+    }
+    
+    private func setupToolbar() {
+        self.title = String(localized: "Ubah")
+        
+        let backButton = UIBarButtonItem(
+            image: UIImage(systemName: "chevron.left"),
+            style: .plain,
+            target: self,
+            action: #selector(backButtonTapped)
+        )
+        navigationItem.setLeftBarButton(backButton, animated: true)
     }
     
 }
